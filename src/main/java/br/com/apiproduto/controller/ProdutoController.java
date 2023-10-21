3a513f33728cfe6ca4dcd7b2fc00db84c1876561
Fabiano <fabiano.analyst@gmail.com>
2023-10-21 14:30:54 -0300

    commit configuration

--- src/main/java/br/com/apiproduto/controller/ProdutoController.java
+++ src/main/java/br/com/apiproduto/controller/ProdutoController.java
@@ -1,9 +1,36 @@
 package br.com.apiproduto.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.apiproduto.entity.Produto;
+import br.com.apiproduto.service.ProdutoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/produto")
+@AllArgsConstructor
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> buscarTodos(){
+        return ResponseEntity.ok(produtoService.buscarTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> salvar(@RequestBody Produto produto){
+        var novoProduto = produtoService.salvar(produto);
+        return new ResponseEntity<>(produto, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        produtoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
